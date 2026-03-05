1b9a01360858e8cfd25451caa8c854b99f5d86c8
Shriyan <shriyan.sharma@yahoo.com>
2026-03-05 22:55:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@
 import app from './src/app.js';
 import { connectRedis } from './src/config/redis.js';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
     await connectRedis();
